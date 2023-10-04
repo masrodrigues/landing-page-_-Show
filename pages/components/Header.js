494f5d2f414fe12b0e8c6335746e3876ff863238
Marco Rodrigues <masrodrigues@inf.ufrgs.br>
2023-10-04 14:07:35 -0300

    Header

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -4,13 +4,36 @@ import Navbar from './Navbar'
 
 function Header() {
   return (
-    <>
+    <header className="index relative text-zinc-200">
       <Navbar></Navbar>
-    <div 
-    style={{ backgroundImage: "url(/images/hero.jpg)"}} 
-    className="flex h-[calc(100vh-200px)] min-h-[400px] flex-col items-center justify-center bg-cover bg-fixed bg-center">
-    </div>
-    </>
+      <div
+        style={{ backgroundImage: "url(/images/hero.jpg)" }}
+        className="flex h-[calc(100vh-200px)] min-h-[400px] flex-col items-center justify-center bg-cover bg-fixed bg-center"
+      >
+        <div className="rounded-xl bg-white/30 py-2 px-4 text-center backdrop-blur-md">
+          <img src="/images/logo.png" width="175" />
+
+        <div className="text-5xl font-bold">
+            tw:<span className="text-sky-900">mf</span>
+          </div>
+        </div>
+
+        <div className="mt-3 text-sm font-bold">
+          Mantenha-me atualizado com novidades e promoções
+        </div>
+
+        <div className="mt-3 flex gap-2">
+          <input 
+          type="email" 
+          placeholder='Email' 
+          className='rounded-xl border-white/40 bg-white/30 p-2 font-bold text-sky-900 placeholder-zinc-500 caret-pink-500 outline-pink-500 backdrop-blur-md'/>
+          
+          <div className="cursor-pointer rounded-xl bg-pink-500 py-2 px-4 font-bold transition-colors hover:bg-sky-900 hover:shadow-lg">
+            Inscrever-se
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -4,41 +4,39 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <header className="index relative text-zinc-200">
-      <nav>
-        <nav className="sticky top-0 z-10 flex bg-gradient-to-r from-rose-500 to-pink-500">
-          <div className="logo flex items-center gap-2 p-2">
-            <img
-              className="logo"
-              src="/images/logo-inverted.png"
-              alt="site logo"
-              width="50"
-            />
-            <div className="text-2x1 font-bold">
-              tw:<span className="text-sky-900">mf</span>
-            </div>
+    <nav>
+      <nav className="sticky top-0 z-10 flex bg-gradient-to-r from-rose-500 to-pink-500">
+        <div className="logo flex items-center gap-2 p-2">
+          <img
+            className="logo"
+            src="/images/logo-inverted.png"
+            alt="site logo"
+            width="50"
+          />
+          <div className="text-2xl font-bold">
+            tw:<span className="text-sky-900">mf</span>
           </div>
-          <div className="hidden flex-1 items-center justify-end md:flex">
-            <MenuItem
-              text="Home"
-              subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
-            />
-            <MenuItem
-              text="Lineup"
-              subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
-            />
-            <MenuItem
-              text="Ticket"
-              subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
-            />
-            <MenuItem
-              text="Support"
-              subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
-            />
-          </div>
-        </nav>
+        </div>
+        <div className="hidden flex-1 items-center justify-end md:flex">
+          <MenuItem
+            text="Home"
+            subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
+          />
+          <MenuItem
+            text="Lineup"
+            subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
+          />
+          <MenuItem
+            text="Ticket"
+            subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
+          />
+          <MenuItem
+            text="Support"
+            subMenus={["Ticket de 1 dia", "Ticket de 7 dias"]}
+          />
+        </div>
       </nav>
-    </header>
+    </nav>
   );
 }
 
